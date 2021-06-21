a16a50b5835c79feb82cfbac58ffb5e087bff355
hasan28 <hasan@bestcloudfor.me>
2021-06-21 23:53:05 +0300

    initial commit

---
+++ autoencoder.py
@@ -0,0 +1,144 @@
+import numpy as np
+import pandas as pd
+from tensorflow import keras
+from tensorflow.keras import layers
+from matplotlib import pyplot as plt
+
+master_url_root = "https://raw.githubusercontent.com/numenta/NAB/master/data/"
+
+df_small_noise_url_suffix = "artificialNoAnomaly/art_daily_small_noise.csv"
+df_small_noise_url = master_url_root + df_small_noise_url_suffix
+df_small_noise = pd.read_csv(
+    df_small_noise_url, parse_dates=True, index_col="timestamp"
+)
+
+df_daily_jumpsup_url_suffix = "artificialWithAnomaly/art_daily_jumpsup.csv"
+df_daily_jumpsup_url = master_url_root + df_daily_jumpsup_url_suffix
+df_daily_jumpsup = pd.read_csv(
+    df_daily_jumpsup_url, parse_dates=True, index_col="timestamp"
+)
+
+print(df_small_noise.head())
+
+print(df_daily_jumpsup.head())
+
+fig, ax = plt.subplots()
+df_small_noise.plot(legend=False, ax=ax)
+plt.show()
+
+fig, ax = plt.subplots()
+df_daily_jumpsup.plot(legend=False, ax=ax)
+plt.show()
+
+# Normalize and save the mean and std we get,
+# for normalizing test data.
+training_mean = df_small_noise.mean()
+training_std = df_small_noise.std()
+df_training_value = (df_small_noise - training_mean) / training_std
+print("Number of training samples:", len(df_training_value))
+
+TIME_STEPS = 288
+
+# Generated training sequences for use in the model.
+def create_sequences(values, time_steps=TIME_STEPS):
+    output = []
+    for i in range(len(values) - time_steps + 1):
+        output.append(values[i : (i + time_steps)])
+    return np.stack(output)
+
+
+x_train = create_sequences(df_training_value.values)
+print("Training input shape: ", x_train.shape)
+
+model = keras.Sequential(
+    [
+        layers.Input(shape=(x_train.shape[1], x_train.shape[2])),
+        layers.Conv1D(
+            filters=32, kernel_size=7, padding="same", strides=2, activation="relu"
+        ),
+        layers.Dropout(rate=0.2),
+        layers.Conv1D(
+            filters=16, kernel_size=7, padding="same", strides=2, activation="relu"
+        ),
+        layers.Conv1DTranspose(
+            filters=16, kernel_size=7, padding="same", strides=2, activation="relu"
+        ),
+        layers.Dropout(rate=0.2),
+        layers.Conv1DTranspose(
+            filters=32, kernel_size=7, padding="same", strides=2, activation="relu"
+        ),
+        layers.Conv1DTranspose(filters=1, kernel_size=7, padding="same"),
+    ]
+)
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss="mse")
+model.summary()
+
+history = model.fit(
+    x_train,
+    x_train,
+    epochs=50,
+    batch_size=128,
+    validation_split=0.1,
+    callbacks=[
+        keras.callbacks.EarlyStopping(monitor="val_loss", patience=5, mode="min")
+    ],
+)
+
+plt.plot(history.history["loss"], label="Training Loss")
+plt.plot(history.history["val_loss"], label="Validation Loss")
+plt.legend()
+plt.show()
+
+# Get train MAE loss.
+x_train_pred = model.predict(x_train)
+train_mae_loss = np.mean(np.abs(x_train_pred - x_train), axis=1)
+
+plt.hist(train_mae_loss, bins=50)
+plt.xlabel("Train MAE loss")
+plt.ylabel("No of samples")
+plt.show()
+
+# Get reconstruction loss threshold.
+threshold = np.max(train_mae_loss)
+print("Reconstruction error threshold: ", threshold)
+
+# Checking how the first sequence is learnt
+plt.plot(x_train[0])
+plt.plot(x_train_pred[0])
+plt.show()
+
+df_test_value = (df_daily_jumpsup - training_mean) / training_std
+fig, ax = plt.subplots()
+df_test_value.plot(legend=False, ax=ax)
+plt.show()
+
+# Create sequences from test values.
+x_test = create_sequences(df_test_value.values)
+print("Test input shape: ", x_test.shape)
+
+# Get test MAE loss.
+x_test_pred = model.predict(x_test)
+test_mae_loss = np.mean(np.abs(x_test_pred - x_test), axis=1)
+test_mae_loss = test_mae_loss.reshape((-1))
+
+plt.hist(test_mae_loss, bins=50)
+plt.xlabel("test MAE loss")
+plt.ylabel("No of samples")
+plt.show()
+
+# Detect all the samples which are anomalies.
+anomalies = test_mae_loss > threshold
+print("Number of anomaly samples: ", np.sum(anomalies))
+print("Indices of anomaly samples: ", np.where(anomalies))
+
+# data i is an anomaly if samples [(i - timesteps + 1) to (i)] are anomalies
+anomalous_data_indices = []
+for data_idx in range(TIME_STEPS - 1, len(df_test_value) - TIME_STEPS + 1):
+    if np.all(anomalies[data_idx - TIME_STEPS + 1 : data_idx]):
+        anomalous_data_indices.append(data_idx)
+
+df_subset = df_daily_jumpsup.iloc[anomalous_data_indices]
+fig, ax = plt.subplots()
+df_daily_jumpsup.plot(legend=False, ax=ax)
+df_subset.plot(legend=False, ax=ax, color="r")
+plt.show()
